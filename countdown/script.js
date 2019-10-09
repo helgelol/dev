4c8b799daa4d367bd9d4074bf75e4b1c7368df24
masunile <helge@furion.se>
2019-10-10 01:46:01 +0200

    Moved around stuff so it's more correct. And updated the countdown.

--- countdown/script.js
+++ countdown/script.js
@@ -3,7 +3,7 @@ const second = 1000,
       hour = minute * 60,
       day = hour * 24;
 
-let countDown = new Date('Jul 26, 2019 15:30:00').getTime(),
+let countDown = new Date('Nov 16, 2019 18:00:00').getTime(),
     x = setInterval(function() {
 
       let now = new Date().getTime(),
@@ -14,10 +14,4 @@ let countDown = new Date('Jul 26, 2019 15:30:00').getTime(),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
       
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
-
     }, second)
